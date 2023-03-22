cb14a34457fa9e82f782b0184e3f76af2f8a3ae1
Manas <manasverma012345@gmail.com>
2023-03-22 15:55:53 +0530

    update 2.0

--- Flaskwebapp/website/__init__.py
+++ Flaskwebapp/website/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db" # name of the database is given here
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'ghosty'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # data is being stored in the loaction given in the f string in the website folder
+    db.init_app(app) # initialize the database by providing the flask app 
+
+
 
     from .views import views
     from .auth import auth
@@ -11,4 +26,18 @@ def create_app():
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
 
+    from .models import User,Note #function -->(models.py file runs before we create the database)
+    with app.app_context():
+     db.create_all()
+
+    login_manager=LoginManager()
+    login_manager.login_view='auth.login' # where the flask will redirect us if the user is not logged in or there is a login required
+    login_manager.init_app(app) # telling the login manager which app we are using
+
+    @login_manager.user_loader
+    def load_user(id):
+       return User.query.get(int(id))
+
     return app
+
+
---
+++ Flaskwebapp/website/models.py
@@ -0,0 +1,22 @@
+# defining database tables or schema
+
+from . import db # importing current package (website folder) ie: db object
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):  # schema for the particular user data field.
+    id =db.Column(db.Integer,primary_key=True)
+    data=db.Column(db.String(10000))
+    date=db.Column(db.DateTime(timezone=True),default=func.now())
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+
+#note for the lowercase of 'user.id' in line 11 instead of Capital 'User.id' because in python it is a convention to write the classname in Capitals but the sql understands the 'user.id' as 'User.id' where 'User' is a classname (This phenomenon is only valid for foreign key)
+
+# in case for relationship we have to use the Original classname with the Uppecase Character as written in the database schema(here the 'Note')  eg: line 22
+
+class User(db.Model,UserMixin):  # schema of the user sign-up info
+    id =db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(150),unique=True)
+    password=db.Column(db.String(150))
+    first_name=db.Column(db.String(150))
+    notes=db.relationship('Note')
